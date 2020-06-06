8c2ab4418cdcd71cc154c6ee04e7e1301a98404e
luchangyou <hibernate@sina.cn>
2020-06-06 14:31:00 +0800

    初始化提交

---
+++ store-support/src/main/java/com/lcy/store/StoreSupportApplication.java
@@ -0,0 +1,13 @@
+package com.lcy.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSupportApplication.class, args);
+	}
+
+}
