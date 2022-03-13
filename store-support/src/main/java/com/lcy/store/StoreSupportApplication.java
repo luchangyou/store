98e3e5d5d18d52f6f7000e4bac7742ea8900d335
luchangyou <hibernate@sina.cn>
2022-03-13 19:05:30 +0800

    测试git

--- store-support/src/main/java/com/lcy/store/StoreSupportApplication.java
+++ store-support/src/main/java/com/lcy/store/StoreSupportApplication.java
@@ -3,11 +3,17 @@ package com.lcy.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class StoreSupportApplication {
 
 	public static void main(String[] args) {
+
+		//测试git
 		SpringApplication.run(StoreSupportApplication.class, args);
+
 	}
 
 }
